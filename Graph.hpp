820121611a002352cbf4d1b7d43eb58b56796aab
THF5000 <56789071+Lowdham@users.noreply.github.com>
2020-11-22 15:19:00 +0800

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -3,6 +3,7 @@
 
 #include<optional>
 #include<bitset>
+#include<tuple>
 #include<map>
 #include<array>
 #include<list>
@@ -18,123 +19,201 @@ __DS_IMPL_BEGIN
 
 // Here set the maximum of the graph's size and weight type.
 // Default maximum size is 2^16 - 1 = 65535.
-// Default weight type is signed integer.
+// Default weight type is 8bit signed integer.
 using size_t = uint16_t;
+using index_t = uint16_t;
 using weight_t = int16_t;
 
-// Nodes in graphs
-template<typename _Ty>
-struct node_w
-{
-	// index
-	int index;
+constexpr weight_t default_weight = 1;
+constexpr weight_t disconnected = -1;
+#define WEIGHT_CHECK(x) x > disconnected
 
-	// weight
+
+//
+template<bool _Weighted>
+struct linkNode { };
+
+template<>
+struct linkNode<true>
+{
+	//destination's index
+	index_t index;
+
+	//path weight
 	weight_t weight;
 
-	// data
-	_Ty element;
+	linkNode* next;
+
+	linkNode(index_t i, weight_t w, linkNode* n = nullptr) :index(i), weight(w), next(n) { }
 };
 
-template<typename _Ty>
-struct node
+template<>
+struct linkNode<false>
 {
-	// index
-	size_t index;
+	//destination's index
+	index_t index;
 
-	// data
-	_Ty element;
+	linkNode* next;
+
+	linkNode(index_t i, linkNode* n = nullptr) :index(i), next(n) { }
 };
 
 // Size is extendable 
-template<typename _Ty, bool _Weighted, bool _Oriented>
+template<typename _Ty,
+		 bool _Weighted = false,
+		 bool _Oriented = false
+>
 class list
 {
-	//std::map<size_t,
-};
+	using _Link_type = linkNode<_Weighted>;
+	using _Link = _Link_type*;
+	using _Node = std::pair<_Ty, _Link>;
 
-/**/
-template<typename _Ty,bool _Weighted, bool _Oriented, size_t _Size>
-class matrix_impl { };
-
-template<typename _Ty, size_t _Size, bool _Oriented>
-class matrix_impl<_Ty,true,_Oriented,_Size>
-{
+	std::map<index_t, _Node> adjacent_list;
 public:
-	matrix_impl()
+	list():adjacent_list()
 	{
-		for (auto& i : adjacent_matrix)
-			for (auto& j : i)
-				j = -1;
+		
 	}
 
-	void insertEdge(size_t i, size_t j, int weight = 1) noexcept
+	std::optional<_Ty> at(index_t id) const
 	{
-		adjacent_matrix[i][j] = weight;
-		if constexpr (_Oriented)
-			return;
+		if (!adjacent_list.count(id))
+			return std::nullopt;
+		
+		return adjacent_list.find(id)->second.first;
+	}
+
+	void insertNode(index_t index, const _Ty& value)
+	{
+		//change the value if the node already exists
+		if (adjacent_list.count(index))
+			adjacent_list[index].first = value;
 		else
-			adjacent_matrix[j][i] = weight;
+		{
+			adjacent_list[index].first = value;
+			adjacent_list[index].second = nullptr;
+		}
 	}
-protected:
-	std::array<std::array<size_t, _Size>, _Size> adjacent_matrix;
-};
 
-template<typename _Ty, size_t _Size,bool _Oriented>
-class matrix_impl<_Ty,false, _Oriented,_Size>
-{
+	void insertEdge(index_t s, index_t d, weight_t weight = default_weight, bool firstInsert = true)
+	{
+		_Link current = adjacent_list[s].second;
+
+		if (!current)
+		{
+			if constexpr (_Weighted)
+				adjacent_list[s].second = new _Link_type(d, weight);
+			else
+				adjacent_list[s].second = new _Link_type(d);
+			if constexpr (!_Oriented)
+				if (firstInsert)
+					insertEdge(d, s, weight, false);
+			return;
+		}
+
+		while (current && current->next)
+		{
+			//check if it already exists
+			if (current->index == d)
+			{
+				// change its weight and return (only in weighted graph)
+				// just return (in non-weighted graph)
+				if constexpr (_Weighted)
+					current->weight = weight;
+				if constexpr (!_Oriented)
+					if (firstInsert)
+						insertEdge(d, s, weight, false);
+				return;
+			}
+			current = current->next;
+		}
+		if constexpr (_Weighted)
+			current->next = new _Link_type(d, weight);
+		else
+			current->next = new _Link_type(d);
+		if constexpr (!_Oriented)
+			if (firstInsert)
+				insertEdge(d, s, weight, false);
+
+	}
+
+	weight_t weightOfEdge(index_t s, index_t d)
+	{
+
+	}
+private:
+	bool indexCheck(size_t id) const
+	{
+		
+	}
 public:
-	matrix_impl()
+	// traversal in the linking list of a vertices
+	template<typename _Function>
+	void traversal(index_t s, _Function&& f)
 	{
-		for (size_t i = 0; i < _Size; ++i)
-			adjacent_matrix[i].reset();
-	}
-
-	void insertEdge(size_t i, size_t j) noexcept
-	{
-		adjacent_matrix[i][j] = 1;
-		if constexpr (_Oriented)
+		// return if the node or it's link doesn't exist
+		if (!adjacent_list.count(s))
 			return;
-		else
-			adjacent_matrix[j][i] = 1;
+
+		_Link current = adjacent_list.find(s)->second.second;
+		while (current)
+		{
+			f(current);
+			current = current->next;
+		}
 	}
-protected:
-	std::bitset<_Size> adjacent_matrix[_Size];
 };
 
 // Weighted graph use std::array as its two dimension matrix.
 // Non-weighted graph use std::bitset as its two dimension matrix.
-template<typename _Ty,bool _Weighted,bool _Oriented,size_t _Size>
-class matrix:public matrix_impl<_Ty, _Weighted, _Oriented,_Size>
+template<typename _Ty,
+		 bool _Weighted = false,
+		 bool _Oriented = false,
+		 size_t _Size = 6
+>
+class matrix
 {
-	using base = matrix_impl<_Ty, _Weighted, _Oriented, _Size>;
-	using base::adjacent_matrix;
-	using _Elem = std::optional<_Ty>;
-	std::array<_Elem, _Size> vertices;
+	using matrix_nonweighted_type = std::bitset<_Size>;
+	using matrix_weighted_type = std::array<std::array<weight_t, _Size>, _Size>;
+	using matrix_type = std::tuple_element_t<_Weighted,
+											std::tuple<matrix_nonweighted_type, matrix_weighted_type>>;
+	matrix_type adjacent_matrix;
+	std::array<_Ty, _Size> vertices;
 public:
-	matrix():vertices()
+	matrix():vertices(), adjacent_matrix()
 	{
-		for (auto& i : vertices)
-			i = std::nullopt;
+		if constexpr (_Weighted)
+			for (size_t i = 0; i < _Size; ++i)
+				for (size_t j = 0; j < _Size; ++j)
+					adjacent_matrix[i][j] = disconnected;
+		else
+			for (size_t i = 0; i < _Size; ++i)
+				adjacent_matrix[i].reset();
 	}
 
-	_Elem at(size_t id) const noexcept
+	const _Ty& at(size_t id) const noexcept
 	{ 
-		if (vertices[id].has_value())
-			return vertices[id];
-		else
-			return std::nullopt;
+		return vertices[id];
+	}
+
+	void setValue(size_t id, const _Ty& value)
+	{
+		vertices[id] = value;
 	}
 
 	template<typename... _Arg>
 	void emplace(size_t id, _Arg&&... args)
 	{
-		vertices[id].emplace(std::forward<_Arg>(args)...);
+			vertices[id] = _Ty(std::forward<_Arg>(args)...);
 	}
 
 	void erase(size_t id)
 	{
-		vertices[id].reset();
+		if constexpr (std::is_pointer_v<_Ty>)
+			vertices[id] = nullptr;
+		else
+			vertices[id] = _Ty();
 	}
 
 	size_t numberOfVertices() const noexcept
@@ -149,15 +228,6 @@ public:
 		for (size_t i = 0; i < _Size; ++i)
 			edge += degree(i);
 		return edge / 2;
-	}
-
-	size_t numberOfValid() const noexcept
-	{
-		size_t count = 0;
-		for (auto& i : vertices)
-			if (i != std::nullopt)
-				++count;
-		return count;
 	}
 
 	size_t degree(size_t id) const noexcept
@@ -184,7 +254,7 @@ public:
 			{
 				if constexpr (_Weighted)
 				{
-					if (adjacent_matrix[i][id] > -1)
+					if (WEIGHT_CHECK(adjacent_matrix[i][id]))
 						++count;
 				}
 				else
@@ -204,7 +274,7 @@ public:
 			for (size_t i = 0; i < _Size; ++i)
 				if constexpr (_Weighted)
 				{
-					if (adjacent_matrix[id][i] > -1)
+					if (WEIGHT_CHECK(adjacent_matrix[id][i]))
 						++count;
 				}
 				else
@@ -218,17 +288,35 @@ public:
 	bool existsEdge(size_t i, size_t j) const noexcept
 	{
 		if constexpr (_Weighted)
-			return adjacent_matrix[i][j] > -1 ? true : false;
+			return WEIGHT_CHECK(adjacent_matrix[i][j]) ? true : false;
 		else
 			return adjacent_matrix[i][j];
 	}
 
-	using base::insertEdge;
+	void insertEdge(size_t i, size_t j, weight_t weight = default_weight) noexcept
+	{
+		if constexpr (_Weighted)
+		{
+			adjacent_matrix[i][j] = weight;
+			if constexpr (_Oriented)
+				return;
+			else
+				adjacent_matrix[j][i] = weight;
+		}
+		else
+		{
+			adjacent_matrix[i][j] = 1;
+			if constexpr (_Oriented)
+				return;
+			else
+				adjacent_matrix[j][i] = 1;
+		}
+	}
 
 	void eraseEdge(size_t i, size_t j) noexcept
 	{
 		if constexpr (_Weighted)
-			insertEdge(i, j, -1);
+			insertEdge(i, j, disconnected);
 		else
 		{
 			adjacent_matrix[i][j] = 0;
@@ -239,7 +327,7 @@ public:
 		}
 	}
 
-	int weightOfEdge(size_t i, size_t j)
+	weight_t weightOfEdge(size_t i, size_t j)
 	{
 		if constexpr (_Weighted)
 		{
@@ -251,17 +339,18 @@ public:
 };
 
 template<typename _Ty,
-	bool _Weighted = false,
-	bool _Oriented = false,
-	bool _Matrix = false,
-	size_t _Size = 0
+	 	 bool _Weighted = false,
+		 bool _Oriented = false,
+		 bool _Matrix = false,
+		 size_t _Size = 0
 >
 class graph_impl { };
 
 template<typename _Ty,
 		 bool _Weighted,
 		 bool _Oriented,
-		 size_t _Size>
+		 size_t _Size
+>
 class graph_impl<_Ty, _Weighted, _Oriented, true, _Size> :
 	public matrix<_Ty, _Weighted, _Oriented, _Size>
 {
@@ -270,9 +359,10 @@ public:
 };
 
 template<typename _Ty,
-	bool _Weighted,
-	bool _Oriented,
-	size_t _Size>
+		 bool _Weighted,
+		 bool _Oriented,
+		 size_t _Size
+>
 class graph_impl<_Ty, _Weighted, _Oriented, false, _Size> :
 	public list<_Ty, _Weighted, _Oriented>
 {
@@ -289,7 +379,12 @@ using ds_impl::size_t;
 /*
  *	The implement of graph can be based on adjacent matrix or adjacent list.
 */
-template<typename _Ty, bool _Weighted = false, bool _Oriented = false, bool _Matrix = true, size_t _Size = 0>
+template<typename _Ty,
+		 bool _Weighted = false,
+		 bool _Oriented = false,
+		 bool _Matrix = true,
+		 size_t _Size = 0
+>
 class graph :public graph_impl<_Ty, _Weighted, _Oriented, _Matrix, _Size>
 {
 public:
