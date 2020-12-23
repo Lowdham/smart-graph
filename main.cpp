814e49fcaac62d60c6b7decf8bbc581915a4ba4a
THF5000 <56789071+Lowdham@users.noreply.github.com>
2020-12-23 22:54:44 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,55 @@
-#include "Graph.hpp"
-#include <iostream>
-using namespace ds;
+#include "../include/graph.hpp"
+using namespace smart_graph;
 
 int main()
 {
-    graph<index_t,true,true,false> list_graph;
-    graph<index_t,false,true,true,10> matrix_graph;
+    Graph<index_t,true,true,false,15> list_graph;
+    Graph<index_t,false,true,true,10> matrix_graph;
     for(index_t i =0; i< 10; i++)
-        list_graph.registerVertices(i,i);
-    for(index_t i=0 ; i< 10;++i)
-        for(index_t j=0 ; j< 10;++j)
-        list_graph.insertEdge(i,i - j,j);
+        list_graph.RegisterVertex(i,i);
 
-    std::vector<decltype(list_graph.edge_type())> cont;
-    list_graph.getEdgeOut(1,cont);
-    list_graph.getEdgeOut(5,cont,true);
-    list_graph.getEdgeOut(2,cont,true);
-    list_graph.getEdgeOut(3,cont,true);
-    list_graph.getEdgeOut(4,cont,true);
-    list_graph.getEdgeOut(8,cont,true);
-    list_graph.getEdgeOut(6,cont,true);
-    list_graph.getEdgeOut(7,cont,true);
-    list_graph.getEdgeOut(9,cont,true);
-    auto plainGraph = make_plaingraph(matrix_graph,cont);
-    auto subGraph = make_subgraph(plainGraph,1,7,9);
-    plainGraph.print();
-    std::cout << "======================\n";
-    subGraph.print();
+    
+    list_graph.InsertEdge(0, 1);
+    list_graph.InsertEdge(0, 2);
+    list_graph.InsertEdge(0, 3);
+    list_graph.InsertEdge(1, 2);
+    list_graph.InsertEdge(1, 3);
+    list_graph.InsertEdge(2, 1,13);
+    list_graph.InsertEdge(2, 3,5);
+    list_graph.InsertEdge(3, 1);
+    list_graph.InsertEdge(3, 2);
 
-    std::cout << list_graph.debug_info() << matrix_graph.debug_info() << plainGraph.debug_info() << subGraph.debug_info();
-    system("pause");
+
+    std::vector<decltype(list_graph.GetEdgeType())> cont;
+    list_graph.GetEdgeOut(1,cont);
+    list_graph.GetEdgeOut(0,cont,true);
+    list_graph.GetEdgeOut(2,cont,true);
+    list_graph.GetEdgeOut(3,cont,true);
+
+    list_graph.Print();
+    std::cout << "======================\nPlaingraph Output\n";
+    auto plainGraph = MakePlaingraph(matrix_graph,cont);
+    plainGraph.Print();
+    std::cout << "======================\nSubgraph Output\n";
+    auto subGraph = MakeSubgraph(plainGraph, 1, 3, 0);
+    subGraph.Print();
+    std::cout << "======================\nPlain Subgraph Output\n";
+    auto plainSubgraph = MakePlainSubgraph(list_graph, 1, 3, 0);
+    plainSubgraph.Print();
+
+    std::cout << list_graph.DebugInfo() << matrix_graph.DebugInfo() << plainGraph.DebugInfo() << subGraph.DebugInfo() << std::endl;
+    std::cout << "success\0" << std::endl;
+
+    auto queue = list_graph.makeEdgeQueue<true>();
+    list_graph.GetEdgeOutOrdered(2,queue);
+    list_graph.GetEdgeOutOrdered(0,queue,true);
+
+    while(!queue.empty())
+    {
+        auto first = queue.top();
+        std::cout << "[" << first.start_ << "]->[" << first.destination_  << "]:" << first.weight_ << "\n";
+        queue.pop();
+    }
+
     return 0;
 }
